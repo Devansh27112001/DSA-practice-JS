f143f78ce04f60b2d455f9f989d6532a570c970b
Devansh27112001 <dkansara2711@gmail.com>
2024-08-27 13:29:30 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,48 @@
+// Count the vowels
+const string = "HEY js. You are Amazing";
+const vowels = ["a", "e", "i", "o", "u"];
+const countVowels = string
+  .toLowerCase()
+  .split("")
+  .filter((character) => vowels.includes(character)).length;
+console.log(countVowels);
+
+// Count the occurence of each character
+const string1 = "hare krishna hare krishna hare hare";
+const object = {};
+
+const charCount = [...string1]
+  .filter((character) => character !== " ")
+  .reduce((a, c) => {
+    a[c] = (a[c] || 0) + 1;
+    return a;
+  }, object);
+
+charCount;
+
+// Objects deep copy
+const persondata = {
+  name: "Devansh",
+  age: 55,
+  details: {
+    religion: "Hindu",
+  },
+};
+persondata;
+
+// const copyPersonData = Object.assign({}, persondata);
+// Creates a shallow copy
+const changeObject = (obj) => {
+  const newObject = { ...obj };
+  newObject.details.religion = 63;
+  return newObject;
+};
+const copyPersonData = changeObject(persondata);
+
+copyPersonData;
+persondata;
+
+// Event loop and microtask loop
+console.log("Devansh1");
+setTimeout(() => console.log("Timer 1"), 0);
+console.log("Devansh");
