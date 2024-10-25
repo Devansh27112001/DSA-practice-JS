5894f603cfe544c4e8d25019418600cd5806ffc7
Devansh27112001 <dkansara2711@gmail.com>
2024-10-25 18:58:34 -0400

    WEEK 6 done

---
+++ palindromeIndex.js
@@ -0,0 +1,22 @@
+const isPalidrome = (string) => {
+  if (s === s.split("").reverse().join("")) {
+    return true;
+  }
+  return false;
+};
+
+const paindromeIndex = (string) => {
+  const n = string.length;
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    if (string[i] !== string[n - 1 - i]) {
+      if (isPalidrome(string.slice(i + 1, n - i))) {
+        return i;
+      }
+      if (isPalidrome(string.slice(i, n - i - 1))) {
+        return n - i - 1;
+      }
+      return -1;
+    }
+  }
+  return -1;
+};
