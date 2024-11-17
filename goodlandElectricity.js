193ea8aa5bcede3390530927df80fe2065e6cd91
Devansh27112001 <dkansara2711@gmail.com>
2024-11-17 12:25:11 -0500

    WEEK 7: Goodland electricity --> Good question(took me some time to find the edge cases)

---
+++ goodlandElectricity.js
@@ -0,0 +1,22 @@
+const minimumPowerPlants = (k, arr) => {
+  const n = arr.length;
+  let count = 0;
+  let i = 0;
+  let location = i + (k - 1);
+  while (i < n) {
+    if (arr[location] === 1) {
+      i = location + k;
+      location = i + (k - 1);
+      count++;
+      if (location >= n) {
+        location = n - 1;
+      }
+    } else {
+      location -= 1;
+      if (location < i - (k + 1) || location < 0) {
+        return -1;
+      }
+    }
+  }
+  return count;
+};
